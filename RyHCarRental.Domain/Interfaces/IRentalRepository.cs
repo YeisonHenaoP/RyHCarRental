e9a3fab7256028a374014fd957af13bbcb1072c8
yeison_prestamo <yeisonhenao107513@correo.itm.edu.co>
2026-05-27 20:35:50 -0500

    creation of interfaces

---
+++ RyHCarRental.Domain/Interfaces/IRentalRepository.cs
@@ -0,0 +1,10 @@
+﻿using RyHCarRental.Domain.Entities;
+
+namespace RyHCarRental.Domain.Interfaces
+{
+    public interface IRentalRepository : IGenericRepository<Rental>
+    {
+        Task<IEnumerable<Rental>> GetActiveRentalsByCustomerAsync(int customerId);
+        Task<bool> IsVehicleAvailableAsync(int vehicleId, DateTime start, DateTime end);
+    }
+}
