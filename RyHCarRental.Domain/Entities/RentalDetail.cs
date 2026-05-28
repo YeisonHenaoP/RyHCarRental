0ce0c135d6efd4eb25d0a9011d8131c591afa25b
yeison_prestamo <yeisonhenao107513@correo.itm.edu.co>
2026-05-27 20:26:58 -0500

    creation of enums and entities: customer, rental and rentaldetail

---
+++ RyHCarRental.Domain/Entities/RentalDetail.cs
@@ -0,0 +1,14 @@
+﻿namespace RyHCarRental.Domain.Entities
+{
+    public class RentalDetail
+    {
+        public int Id { get; set; }
+        public int RentalId { get; set; }
+        public int VehicleId { get; set; }
+        public decimal SubTotal { get; set; }
+
+        // Propiedades de navegación
+        public Rental Rental { get; set; } = null!;
+        public Vehicle Vehicle { get; set; } = null!;
+    }
+}
