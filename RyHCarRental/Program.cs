619859a37e75bdbef9851ba4b82fa762a88e172e
yeison_prestamo <yeisonhenao107513@correo.itm.edu.co>
2026-05-27 21:16:28 -0500

    are update program and appsettings.json

--- RyHCarRental/Program.cs
+++ RyHCarRental/Program.cs
@@ -1,11 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using RyHCarRental.API.Mappings;
+using RyHCarRental.API.Services;
+using RyHCarRental.DataAccess.Context;
+using RyHCarRental.DataAccess.Repositories;
+using RyHCarRental.Domain.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Repositories
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IRentalRepository, RentalRepository>();
+
+// Add Services
+builder.Services.AddScoped<RentalService>();
+
+// Add AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
 
